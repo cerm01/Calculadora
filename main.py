f0fcc893a310f30439e19fbcafda72291370c16e
Efrain Rosas <carlos.efrain.rosas.medina@gmail.com>
2022-08-31 08:46:39 -0500

    Cambio de nombres de variables, forma de validar los datos ingresado, cambio de parametros en la funcion validacion

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import os
 def captura():
     programadores = list()
     operaciones = list()
-    primerosperando = list()
-    segundosoperando = list()
-    tme = list()
-    Id = list()
+    primerosperandos = list()
+    segundosoperandos = list()
+    tme.s = list()
+    Ids = list()
     x = 1
     y = 1
     #Limpiar pantalla
     os.system("cls")
     
-    while x != 1:
+    while x != 0:
         while y != 0:
             programador = input("Nombre del programador: ")
             operacion = input("Operación: ")
@@ -20,20 +20,28 @@ def captura():
             segundooperando = int(input("Segundo operando: "))
             tme = int(input("Tiempo de ejecución: "))
             Id = int(input("Id: "))
-            if validacion(operacion, primeroperando, segundooperando, tme, Id) == 0:
-                y = 0     
-            else:
+
+            if validacion(operacion, segundooperando, tme, Id, Ids) == 0:
                 programadores.append(programador)
                 operaciones.append(operacion)
-                primerosperando.append(primeroperando)
-                segundosoperando.append(segundooperando)
-                tme.append(tme)
-                Id.append(Id)
+                primerosperandos.append(primeroperando)
+                segundosoperandos.append(segundooperando)
+                tme.s.append(tme)
+                Ids.append(Id)
+            else: 
+                print("No se agregó el proceso") 
                 
         x = int(input("¿Desea capturar otro proceso? 1.- Si 2.- No: "))
+        if x == 1:
+            pass
+        elif x == 2:
+            x = 0
+        else:
+            print("Opción no válida")
+            x = 0
 
 
-def validacion(operacion, segundooperando, tme, Id):
+def validacion(operacion, segundooperando, tme, Id, Ids):
     valor = 0
     if operacion != ("+" or "-" or "*" or "/" or "//" or "**"):
         print("Operación no válida")
@@ -47,7 +55,7 @@ def validacion(operacion, segundooperando, tme, Id):
         print("El tiempo de ejecución debe ser mayor a 0")
         valor += 1
     
-    for i in range(len(Id)):
+    for i in range(0, len(Ids)):
         if Id[i] == Id:
             print("El Id ya existe")
             valor += 1
@@ -61,7 +69,14 @@ def menu():
 
 
 def main():
-    pass
+    menu()
+    opcion = int(input("Seleccione una opción: "))
+    if opcion == 1:
+        captura()
+    elif opcion == 2:
+        pass
+    else:
+        print("Opción no válida")
 
 
 if __name__ == '__main__':

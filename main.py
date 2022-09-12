54e1c28071360ee0cee761001a653d66f367dcc0
Efrain Rosas <carlos.efrain.rosas.medina@gmail.com>
2022-09-11 23:16:07 -0500

    Manera de impresion de los datos, puesta en marcha del contador general del tiempo y demas detalles de la impresion en pantalla

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def captura(programadores, operaciones, primerosperandos, segundosoperandos, tmes, Ids):
     x = 1
@@ -8,7 +9,7 @@ def captura(programadores, operaciones, primerosperandos, segundosoperandos, tme
         print("Captura de procesos")
         while y != 0:
             programador = input("Nombre del programador: ")
-            print("Operaciones validas : +, -, *, /, //, **")
+            print("Operaciones validas : +, -, *, /, % , **")
             operacion = str(input("Operación: "))
             primeroperando = int(input("Primer operando: "))
             segundooperando = int(input("Segundo operando: "))
@@ -43,13 +44,13 @@ def captura(programadores, operaciones, primerosperandos, segundosoperandos, tme
 
 def validacion(operacion, segundooperando, tme, Id, Ids):
     valor = 0
-    if operacion == "+" or operacion == "-" or operacion == "*" or operacion == "/" or operacion == "//" or operacion =="**":
+    if operacion == "+" or operacion == "-" or operacion == "*" or operacion == "/" or operacion == "%" or operacion =="**":
         pass
     else:
         print("Operación no válida")
         valor += 1
 
-    if (operacion == "/" or operacion == "//") and segundooperando == 0:
+    if (operacion == "/" or operacion == "%") and segundooperando == 0:
         print("No se puede dividir entre 0")
         valor += 1
 
@@ -65,8 +66,115 @@ def validacion(operacion, segundooperando, tme, Id, Ids):
     return valor
 
 
+def operacion(operacion, primeroperando, segundooperando):
+    if operacion == "+":
+        return primeroperando + segundooperando
+    elif operacion == "-":
+        return primeroperando - segundooperando
+    elif operacion == "*":
+        return primeroperando * segundooperando
+    elif operacion == "/":
+        return primeroperando / segundooperando
+    elif operacion == "%":
+        return primeroperando % segundooperando
+    elif operacion == "**":
+        return primeroperando ** segundooperando
+
+
 def procesar(programadores, operaciones, primerosperandos, segundosoperandos, tmes, Ids):
-    pass
+    lotes = 0
+    acumulador = 0
+    tt = 0
+    tr = 0
+    tme = 0
+
+    contador_global = 0
+    cont_lotes = 0
+    x = 0  
+    cont = 0
+    lote_ejecucion = list()
+    procesos_finalizados = list()
+
+    for i in range(0, len(tmes)):
+        tme += tmes[i]
+    resultado = list()
+
+    for i in range(0, len(Ids)): 
+        acumulador += 1
+        if acumulador == 3:
+            lotes += 1
+            acumulador = 0
+        elif i == len(Ids) - 1 and acumulador != 0:
+            lotes += 1   
+
+    while cont_lotes < lotes: 
+        for j in range(x, len(Ids)):
+            cont += 1
+            if cont <= 3:
+                lote_ejecucion.append(j)
+            if cont == 3:
+                cont = 0
+                x = j + 1
+                break
+        
+        for i in range(0, len(lote_ejecucion)):
+            proceso_ejecucion = lote_ejecucion.pop(0)
+            tme = tmes[proceso_ejecucion]
+            while tt <= tme:
+                print("Lotes pendientes: ", lotes - cont_lotes)
+                print("")
+                print("-----------------")
+                print("Lote en ejecución")
+                print("-----------------")
+                print("")
+                for i in range (0, len(lote_ejecucion)):
+                    print("Programador", programadores[lote_ejecucion[i]] ,"\t","ID: ", Ids[lote_ejecucion[i]], "\t", "TME: ", tmes[lote_ejecucion[i]])
+
+                print("")
+                print("--------------------")
+                print("Proceso en ejecución")
+                print("--------------------")
+                print("")
+                print("Programador: ", programadores[proceso_ejecucion])
+                print("Operacion: ", primerosperandos[proceso_ejecucion], operaciones[proceso_ejecucion], segundosoperandos[proceso_ejecucion])
+                print("ID: ", Ids[proceso_ejecucion])
+                print("TME: ", tmes[proceso_ejecucion])
+                print("TT: ", tt)
+                print("TR: ", tmes[proceso_ejecucion] - tt)
+                
+                print("")
+                print("-------------------")
+                print("Procesos finalizado")
+                print("-------------------")
+                print("")
+                for j in range(0, len(procesos_finalizados)):
+                    print("ID: ", Ids[procesos_finalizados[j]], "\t", "Operacion: ", primerosperandos[procesos_finalizados[j]], operaciones[procesos_finalizados[j]], segundosoperandos[procesos_finalizados[j]], "\t", "Resultado: ", operacion(operaciones[procesos_finalizados[j]], primerosperandos[procesos_finalizados[j]], segundosoperandos[procesos_finalizados[j]]))
+                
+                time.sleep(1)
+                tt += 1
+                contador_global += 1 
+                os.system("cls")
+                
+            tt = 0
+            procesos_finalizados.append(proceso_ejecucion)
+            
+        cont_lotes += 1            
+    print("-----------------")
+    print("Lote en ejecución")
+    print("-----------------")
+
+    print("--------------------")
+    print("Proceso en ejecución")
+    print("--------------------")
+
+    print("-------------------")
+    print("Procesos finalizado")
+    print("-------------------")
+    for j in range(0, len(procesos_finalizados)):
+        print("ID: ", Ids[procesos_finalizados[j]], "\t", "Operacion: ", primerosperandos[procesos_finalizados[j]], operaciones[procesos_finalizados[j]], segundosoperandos[procesos_finalizados[j]], "\t", "Resultado: ", operacion(operaciones[procesos_finalizados[j]], primerosperandos[procesos_finalizados[j]], segundosoperandos[procesos_finalizados[j]]))
+    print("")
+    print("Tiempo total de ejecución: ", contador_global)
+    os.system("pause")
 
 
 def menu():
@@ -83,8 +191,7 @@ def validacion_menu(opcion):
     else:
         opcion = int(opcion)
     
-    return opcion
-    
+    return opcion 
 
 
 def main():
@@ -94,7 +201,10 @@ def main():
     segundosoperandos = list()
     tmes = list()
     Ids = list()
+    
+
     opcion = 0
+    
 
     while opcion != 2:
         menu()
